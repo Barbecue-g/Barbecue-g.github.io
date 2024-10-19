85248fdc9f9fdde1a74248735d8ec1252d153464
Barbecue <2210016303@qq.com>
2024-10-19 15:51:41 +0800

    Site updated: 2024-10-19 15:51:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/06/23/MySQL使用方法/","2023/06/22/MySQL配置/","2023/07/28/Redis的配置/","2023/06/22/Linux创建用户/","2023/04/04/Python/","2024/10/22/hello-world/","2023/06/22/git/","2023/08/20/Nginx/","2023/08/16/HTTP/"];function toRandomPost(){
+var posts=["2023/06/22/Linux创建用户/","2023/08/16/HTTP/","2023/06/23/MySQL使用方法/","2023/06/22/MySQL配置/","2023/07/28/Redis的配置/","2023/04/04/Python/","2024/02/26/Tmux/","2023/06/22/git/","2024/10/22/hello-world/","2023/08/20/Nginx/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
